a120cfbcfbfca50601478e1ac95efddef1890a1f
xibsx <xibstech@gmail.com>
2026-03-07 01:34:48 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -5,45 +5,45 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "POPKID;;;eUlUnY4a#HJzMd2ldQBVM_A-f9ZHJ2hWhOZC9SHN1-URlI4_18fc",
+    SESSION_ID: process.env.SESSION_ID || "BLAZE~mFU1zDoA#GxVY2tGPY9vwY-X9W0ehEgQpOzzy-o4vlSBmFhv-Hdw",
     // add your Session Id
     AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
     // make true or false status auto seen
     AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
     // make true if you want auto reply on status
-    AUTO_BIO: process.env.AUTO_BIO || "true", // ture to get auto bio
+    AUTO_BIO: process.env.AUTO_BIO || "false", // ture to get auto bio
     AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
     // make true if you want auto reply on status
-    AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || ".𝗦𝗘𝗘𝗡 𝗬𝗢𝗨𝗥 𝗦𝗧𝗔𝗧𝗨𝗦 𝗕𝗬 𝗣𝗢𝗣𝗞𝗜𝗗 𝗫𝗧𝗥🔄",
+    AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "፝",
     // set the auto reply massage on status reply
     ANTI_LINK: process.env.ANTI_LINK || "true",
     // make anti link true,false for groups
-    MENTION_REPLY: process.env.MENTION_REPLY || "true",
+    MENTION_REPLY: process.env.MENTION_REPLY || "false",
     // make true if want auto voice reply if someone menetion you
-    MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/rw0yfd.png",
+    MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/zum6dw.jpg",
     // add custom menu and mention reply image url
     PREFIX: process.env.PREFIX || ".",
     // add your prifix for bot
-    BOT_NAME: process.env.BOT_NAME || "NYX MD",
+    BOT_NAME: process.env.BOT_NAME || "G͔̕L̫ͥ̀O̵̠͆̇ͮB͎̂̇͢͟͝A̷̴̯͓̍̿Ḽ̶̵̷ͩ͡",
     // add bot namw here for menu
-    STICKER_NAME: process.env.STICKER_NAME || "NYX-MD",
+    STICKER_NAME: process.env.STICKER_NAME || "G͔̕L̫ͥ̀O̵̠͆̇ͮB͎̂̇͢͟͝A̷̴̯͓̍̿Ḽ̶̵̷ͩ͡",
     // type sticker pack name
     CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
     // make this true for custum emoji react
     CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
     // chose custom react emojis by yourself
-    DELETE_LINKS: process.env.DELETE_LINKS || "false",
+    DELETE_LINKS: process.env.DELETE_LINKS || "true",
     // automatic delete links witho remove member
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "255627417402",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "255778206718",
     // add your bot owner number
     // add your bot owner number
-    OWNER_NAME: process.env.OWNER_NAME || "BLAZE TEAM",
+    OWNER_NAME: process.env.OWNER_NAME || "G͔̕L̫ͥ̀O̵̠͆̇ͮB͎̂̇͢͟͝A̷̴̯͓̍̿Ḽ̶̵̷ͩ͡",
     // add bot owner name
-    DESCRIPTION: process.env.DESCRIPTION || "© NYX MD bot",
+    DESCRIPTION: process.env.DESCRIPTION || "© G͔̕L̫ͥ̀O̵̠͆̇ͮB͎̂̇͢͟͝A̷̴̯͓̍̿Ḽ̶̵̷ͩ͡",
     // add bot owner name
-    ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/rw0yfd.png",
+    ALIVE_IMG: process.env.ALIVE_IMG || "G͔̕L̫ͥ̀O̵̠͆̇ͮB͎̂̇͢͟͝A̷̴̯͓̍̿Ḽ̶̵̷ͩ͡",
     // add img for alive msg
-    LIVE_MSG: process.env.LIVE_MSG || ">NYX MD IS ALIVE😍",
+    LIVE_MSG: process.env.LIVE_MSG || "G͔̕L̫ͥ̀O̵̠͆̇ͮB͎̂̇͢͟͝A̷̴̯͓̍̿Ḽ̶̵̷ͩ͡",
     // add alive msg here
     // Newsletter and links
     NEWSLETTER_JID: process.env.NEWSLETTER_JID || '120363421014261315@newsletter',
@@ -53,7 +53,7 @@ module.exports = {
     DEFAULT_GROUP_JID: process.env.DEFAULT_GROUP_JID || '120363406591837257@g.us',
     // secondary owner (used by .owner and internal checks)
     OWNER_NUMBER2: process.env.OWNER_NUMBER2 || "255754206718",
-    READ_MESSAGE: process.env.READ_MESSAGE || "false",
+    READ_MESSAGE: process.env.READ_MESSAGE || "true",
     // Turn true or false for automatic read msgs
     AUTO_REACT: process.env.AUTO_REACT || "false",
     // make this true or false for auto react on all msgs
@@ -61,17 +61,17 @@ module.exports = {
     // false or true for anti bad words
     MODE: process.env.MODE || "public",
     // make bot public-private-inbox-group
-    ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
+    ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "true",
     // make anti link true,false for groups
     AUTO_VOICE: process.env.AUTO_VOICE || "true",
     // make true for send automatic voices
     AUTO_STICKER: process.env.AUTO_STICKER || "false",
     // make true for automatic stickers
-    AUTO_REPLY: process.env.AUTO_REPLY || "true",
+    AUTO_REPLY: process.env.AUTO_REPLY || "false",
     // make true or false automatic text reply
-    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
     // maks true for always online
-    PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+    PUBLIC_MODE: process.env.PUBLIC_MODE || "false",
     // make false if want private mod
     AUTO_TYPING: process.env.AUTO_TYPING || "true",
     // true for automatic show typing
